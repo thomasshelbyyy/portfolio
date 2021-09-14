9c8210a53133617119ac778b7d5a66a01bafc09d
thomasshelbyyy <87592986+thomasshelbyyy@users.noreply.github.com>
2021-09-14 17:19:47 +0800

    Add files via upload

---
+++ portofolio/script.js
@@ -0,0 +1,6 @@
+const history = document.getElementById('history')
+const isiHis = document.getElementById('#isi-history')
+
+history.addEventListener('click', function() {
+	isiHis.style.display = 'inline'
+});
